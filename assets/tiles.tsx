1b9f54ea1d29c7dba77a77ef7e9b39ef244b5cb9
anthonygedeon <anthony.gedeonn@gmail.com>
2024-10-30 23:58:57 -0500

    feat: create world

---
+++ assets/tiles.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles" tilewidth="16" tileheight="16" tilecount="924" columns="33">
+ <image source="tileset.png" width="528" height="448"/>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="26" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
